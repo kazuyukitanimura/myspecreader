99699cfc43307447acea216b65236a89fdd986fe
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-12-23 01:18:14 -0800

    deleted old client

--- client/myspecreader/app/controllers/loginButton.js
+++
@@ -1,8 +0,0 @@
-var url = arguments[0] || '/auth';
-var previousWin = arguments[1] || 'index';
-
-function doClick(e) {
-  var controller = Alloy.createController('webpage', url, previousWin);
-  controller.getView().open();
-}
-
