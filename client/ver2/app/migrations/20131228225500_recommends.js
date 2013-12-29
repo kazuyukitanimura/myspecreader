8d0f5abf4482ff3fe2d887980e708c4312167ffc
kazuyukitanimura <kazuyukitanimura@gmail.com>
2013-12-28 23:00:44 -0800

    migrations

---
+++ client/ver2/app/migrations/20131228225500_recommends.js
@@ -0,0 +1,13 @@
+migration.up = function(migrator) {
+  migrator.createTable({
+    "columns": {
+      'data': 'TEXT',
+      'state': 'INTEGER', // 0: unread, 1: passed, 2: viewSummary, 3: viewOriginal, 4: markAsUnread, 5: star
+      'id': 'TEXT NOT NULL PRIMARY KEY'
+    }
+  });
+};
+
+migration.down = function(migrator) {
+  migrator.dropTable("recommends");
+};
