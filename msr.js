47fedf65e7a7c8858a36b734e28521c0fb93a4ad
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-01-14 23:32:50 -0800

    rsvp for this.scw

--- msr.js
+++ msr.js
@@ -70,15 +70,16 @@ Msr.prototype = Object.create(Feedly.prototype);
 Msr.prototype._initScw = function(options) {
   var userId = options.id;
   if (userId && (!this.scw || options.forceScw)) {
-    // TODO use https://github.com/caolan/async#parallel or https://github.com/tildeio/rsvp.js
-    // fetch matrices asynchronously
-    redis.loadMatricies(userId, CATEGORIES, function(err, scwOptions) {
-      if (err) {
-        console.error(err); // TODO think what to do with the errors, retry?
-        setImmediate(this._initScw, options); // retry
-      } else {
-        this.scw = new Scw(SCW_PARAMS.ETA, SCW_PARAMS.C, SCW_PARAMS.MODE, scwOptions);
-      }
+    this.scwPromise = new RSVP.Promise(function(resolve, reject) {
+      redis.loadMatricies(userId, CATEGORIES, function(err, scwOptions) {
+        if (err) {
+          console.error(err); // FIXME this will become infinite loop, if redis goes down
+          reject(err);
+          setImmediate(this._initScw.bind(this), options); // retry
+        } else {
+          resolve(this.scw = new Scw(SCW_PARAMS.ETA, SCW_PARAMS.C, SCW_PARAMS.MODE, scwOptions));
+        }
+      }.bind(this));
     }.bind(this));
   }
 };
@@ -110,113 +111,108 @@ Msr.prototype.getRecommends = function(options, callback) {
     callback(new Error('Msr.getRecommends Error'));
     return;
   }
-  this.getStreams(options, function streamsCb(err, data, response) {
-    if (!err) {
-      var l = CATEGORIES.length;
-      var now = Date.now();
-      var items = data.items;
-      var scw = this.scw;
-      if (!scw) {
-        setImmediate(streamsCb, err, data, response);
-        console.error('Msr.getRecommends, this.scw is not defined yet');
-        return;
-      }
-      for (var i = items.length; i--;) {
-        var j = 0;
-        var k = '';
-        var item = items[i];
-        // E.g.
-        //{
-        //  "items": [
-        //    "keywords": [...],
-        //      "title": "...",
-        //      "summary": {
-        //        "content": "..."
-        //      },
-        //      "language": "English", // support only English now
-        //      "published": 1385928000000
-        //      "origin": {
-        //        "streamId": "...",
-        //      },
-        var keywords = item.keywords || []; // key prefix: k
-        var title = item.title || ''; // key prefix: t
-        var summary = (item.summary && item.summary.content) || ''; // key prefix: s
-        var featureVector = {
-          // nomoralize how old it is from the time the user sees it
-          ago: now - item.published | 0,
-          // if item.published is undefined this becomes NaN
-          img: 0 // 0 or 1
-        }; // key: word, val:frequency
-        if ((typeof item.engagement) === 'number') {
-          featureVector.engagement = item.engagement | 0;
-        }
-        if ((typeof item.engagementRate) === 'number') {
-          featureVector.engagementRate = item.engagementRate;
-        }
-        if (item.origin && item.origin.streamId) {
-          k = 'o ' + item.origin.streamId;
-          featureVector[k] = 1;
-        }
-        if (item.language) {
-          k = 'l ' + item.language;
-          featureVector[k] = 1;
-        }
-
-        for (j = keywords.length; j--;) {
-          k = 'k ' + keywords[j].trim().toLowerCase();
-          featureVector[k] = 1; // keyword should not been seen more than once
-        }
-        var titlePieces = tokenize(title);
-        for (j = titlePieces.length; j--;) {
-          k = 't ' + titlePieces[j];
-          featureVector[k] = (featureVector[k] | 0) + 1;
-        }
-        var $ = cheerio.load(summary);
-        var $img = $('img').first();
-        if ($img.length) {
-          item.img = $img.attr('src');
-          featureVector.img = 1;
-        }
-        summary = item.summary = stripHtml(summary);
-        var summaryPieces = tokenize(summary);
-        for (j = summaryPieces.length; j--;) {
-          k = 's ' + summaryPieces[j];
-          featureVector[k] = (featureVector[k] | 0) + 1;
-        }
-        item.featureVector = featureVector;
-        // scw.test() does not guarantee the order when the score is tie. The highest score has to be found in a specified order
-        var scores = scw.calcScores(featureVector);
-        var maxScore = Scw.NON_CATEGORY_SCORE;
-        var maxCategory = Scw.NON_CATEGORY;
-        for (j = 0; j < l; j++) {
-          var category = CATEGORIES[j];
-          if (scores.hasOwnProperty(category)) {
-            var value = scores[category];
-            if (maxScore < value) {
-              maxScore = value;
-              maxCategory = category;
+  this.scwPromise.then(function(scw) {
+    this.getStreams(options, function(err, data, response) {
+      if (!err) {
+        var l = CATEGORIES.length;
+        var now = Date.now();
+        var items = data.items;
+        for (var i = items.length; i--;) {
+          var j = 0;
+          var k = '';
+          var item = items[i];
+          // E.g.
+          //{
+          //  "items": [
+          //    "keywords": [...],
+          //      "title": "...",
+          //      "summary": {
+          //        "content": "..."
+          //      },
+          //      "language": "English", // support only English now
+          //      "published": 1385928000000
+          //      "origin": {
+          //        "streamId": "...",
+          //      },
+          var keywords = item.keywords || []; // key prefix: k
+          var title = item.title || ''; // key prefix: t
+          var summary = (item.summary && item.summary.content) || ''; // key prefix: s
+          var featureVector = {
+            // nomoralize how old it is from the time the user sees it
+            ago: now - item.published | 0,
+            // if item.published is undefined this becomes NaN
+            img: 0 // 0 or 1
+          }; // key: word, val:frequency
+          if ((typeof item.engagement) === 'number') {
+            featureVector.engagement = item.engagement | 0;
+          }
+          if ((typeof item.engagementRate) === 'number') {
+            featureVector.engagementRate = item.engagementRate;
+          }
+          if (item.origin && item.origin.streamId) {
+            k = 'o ' + item.origin.streamId;
+            featureVector[k] = 1;
+          }
+          if (item.language) {
+            k = 'l ' + item.language;
+            featureVector[k] = 1;
+          }
+          for (j = keywords.length; j--;) {
+            k = 'k ' + keywords[j].trim().toLowerCase();
+            featureVector[k] = 1; // keyword should not been seen more than once
+          }
+          var titlePieces = tokenize(title);
+          for (j = titlePieces.length; j--;) {
+            k = 't ' + titlePieces[j];
+            featureVector[k] = (featureVector[k] | 0) + 1;
+          }
+          var $ = cheerio.load(summary);
+          var $img = $('img').first();
+          if ($img.length) {
+            item.img = $img.attr('src');
+            featureVector.img = 1;
+          }
+          summary = item.summary = stripHtml(summary);
+          var summaryPieces = tokenize(summary);
+          for (j = summaryPieces.length; j--;) {
+            k = 's ' + summaryPieces[j];
+            featureVector[k] = (featureVector[k] | 0) + 1;
+          }
+          item.featureVector = featureVector;
+          // scw.test() does not guarantee the order when the score is tie. The highest score has to be found in a specified order
+          var scores = scw.calcScores(featureVector);
+          var maxScore = Scw.NON_CATEGORY_SCORE;
+          var maxCategory = Scw.NON_CATEGORY;
+          for (j = 0; j < l; j++) {
+            var category = CATEGORIES[j];
+            if (scores.hasOwnProperty(category)) {
+              var value = scores[category];
+              if (maxScore < value) {
+                maxScore = value;
+                maxCategory = category;
+              }
             }
           }
+          item.estCategory = maxCategory;
+          if (item.originId) {
+            item.href = item.originId;
+          } else if (item.alternate && item.alternate.length && item.alternate[0].href) {
+            item.href = item.alternate[0].href;
+          }
+          delete item.alternate;
+          delete item.fingerprint;
+          delete item.author;
+          delete item.crawled;
+          delete item.sid;
+          delete item.visual;
         }
-        item.estCategory = maxCategory;
-        if (item.originId) {
-          item.href = item.originId;
-        } else if (item.alternate && item.alternate.length && item.alternate[0].href) {
-          item.href = item.alternate[0].href;
-        }
-        delete item.alternate;
-        delete item.fingerprint;
-        delete item.author;
-        delete item.crawled;
-        delete item.sid;
-        delete item.visual;
+        items.sort(function(a, b) {
+          return CATEGORY_LOOKUP[b.estCategory] - CATEGORY_LOOKUP[a.estCategory] || a.ago - b.ago;
+        });
+        // TODO send only required data
       }
-      items.sort(function(a, b) {
-        return CATEGORY_LOOKUP[b.estCategory] - CATEGORY_LOOKUP[a.estCategory] || a.ago - b.ago;
-      });
-      // TODO send only required data
-    }
-    callback(err, data, response);
+      callback(err, data, response);
+    }.bind(this));
   }.bind(this));
 };
 
@@ -230,77 +226,73 @@ Msr.prototype.postRecommends = function(postBody, callback) {
     callback(new Error('Msr.postRecommends Error'));
     return;
   }
-  var scw = this.scw;
-  if (!scw) {
-    setImmediate(this.postRecommends.bind(this), postBody, callback);
-    console.error('Msr.postRecommends, this.scw is not defined yet');
-    return;
-  }
-  var reads = [];
-  var unreads = [];
-  var stars = [];
-  scw.train(function(trainCallback) {
-    if (!trainCallback) {
-      callback(new Error());
-      return;
-    }
-    var data = postBody.data;
-    for (var i = data.length; i--;) {
-      var datum = data[i];
-      var category = datum.category = CATEGORIES[datum.state];
-      if (category === Scw.NON_CATEGORY || category === 'keepUnread') {
-        unreads.push(datum.id);
-      } else {
-        reads.push(datum.id);
+  this.scwPromise.then(function(scw) {
+    var reads = [];
+    var unreads = [];
+    var stars = [];
+    scw.train(function(trainCallback) {
+      if (!trainCallback) {
+        callback(new Error());
+        return;
       }
-      if (category === 'star') {
-        stars.push(datum.id);
+      var data = postBody.data;
+      for (var i = data.length; i--;) {
+        var datum = data[i];
+        var category = datum.category = CATEGORIES[datum.state];
+        if (category === Scw.NON_CATEGORY || category === 'keepUnread') {
+          unreads.push(datum.id);
+        } else {
+          reads.push(datum.id);
+        }
+        if (category === 'star') {
+          stars.push(datum.id);
+        }
+        trainCallback(datum);
       }
-      trainCallback(datum);
-    }
-  });
-  var promises = {
-    saveMatricies: new RSVP.Promise(function(resolve, reject) {
-      redis.saveMatricies(this._results.id, CATEGORIES, scw.covarianceMatrix, scw.weightMatrix, function(err) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }.bind(this)),
-    markAsRead: new RSVP.Promise(function(resolve, reject) {
-      this.postMarkAsRead(reads, function(err) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }.bind(this)),
-    keepUnread: new RSVP.Promise(function(resolve, reject) {
-      this.postKeepUnread(unreads, function(err) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }.bind(this)),
-    saved: new RSVP.Promise(function(resolve, reject) {
-      this.putSaved(stars, function(err) {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    }.bind(this))
-  };
-  rsvpHash(promises).then(function(results) {
-    callback(null, results);
-  }).
-  catch(function(errors) {
-    callback(errors, {});
-  });
+    });
+    var promises = {
+      saveMatricies: new RSVP.Promise(function(resolve, reject) {
+        redis.saveMatricies(this._results.id, CATEGORIES, scw.covarianceMatrix, scw.weightMatrix, function(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }.bind(this)),
+      markAsRead: new RSVP.Promise(function(resolve, reject) {
+        this.postMarkAsRead(reads, function(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }.bind(this)),
+      keepUnread: new RSVP.Promise(function(resolve, reject) {
+        this.postKeepUnread(unreads, function(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }.bind(this)),
+      saved: new RSVP.Promise(function(resolve, reject) {
+        this.putSaved(stars, function(err) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      }.bind(this))
+    };
+    rsvpHash(promises).then(function(results) {
+      callback(null, results);
+    }).
+    catch(function(errors) {
+      callback(errors, {});
+    });
+  }.bind(this));
 };
