0d00b26addb3956ac03729492b3dbecba0d9672d
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-04-09 02:03:43 -0700

    modal wip

--- client/limily/app/controllers/learnMore.js
+++ client/limily/app/controllers/learnMore.js
@@ -1,5 +1,12 @@
 var args = arguments[0] || {};
 
 function doClick(e) {
-
+  var ModalWindow = Ti.UI.createWindow({
+    width: 200,
+    height: 300,
+    backgroundColor: '#E0FF00'
+  });
+  ModalWindow.open({
+    modal: true
+  });
 }
