db087d48e2f2951aab87f6e6503565cb3579c563
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-04-13 17:32:28 -0700

    stop restart

--- template/setup-node.sh
+++ template/setup-node.sh
@@ -22,9 +22,27 @@ do_start()
   /sbin/start-stop-daemon --start --quiet --pidfile \$PIDFILE --background --exec /bin/bash -- -c "exec \$NODE > \$LOGFILE 2>&1" || { log_daemon_msg " start-stop-daemon failed to run \$NODE"; return 1; }
 }
 
+do_stop()
+{
+  /sbin/start-stop-daemon --stop --name node
+}
+
+do_restart()
+{
+  do_stop
+  sleep 2
+  do_start
+}
+
 case "\$1" in
   start)
     do_start
+    ;;
+  stop)
+    do_stop
+    ;;
+  restart)
+    do_restart
     ;;
   *)
     echo "Usage: nodejs start"
