dbcc8f20d934f4430dcb6a0dff89b54b7756a9d4
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-04-16 04:42:53 -0700

    modal template

--- client/limily/app/controllers/learnMore.js
+++ client/limily/app/controllers/learnMore.js
@@ -2,11 +2,23 @@ var args = arguments[0] || {};
 
 function doClick(e) {
   var ModalWindow = Ti.UI.createWindow({
-    width: 200,
-    height: 300,
-    backgroundColor: '#E0FF00'
+    opacity: 0.9,
+    backgroundColor: '#F7F7F7',
+    top: Ti.Platform.displayCaps.platformHeight
   });
-  ModalWindow.open({
-    modal: true
+  var slide = Titanium.UI.createAnimation();
+  slide.top = 0;
+  ModalWindow.open(slide);
+  var close = Titanium.UI.createButton({
+    title: 'Close'
   });
+  close.addEventListener('click', function(e) {
+    ModalWindow.close();
+    ModalWindow = null;
+  });
+  ModalWindow.add(close);
+  //Ti.UI.createAlertDialog({
+  //  title: 'Welcome to Limily',
+  //  message: 'What is it?\nLimily is an inteligent news feed reader. It learns your preference and reading behavior over time. To save your time, Limily automagically brings the most relevant news at the top of the list. Limily is the best learning tool for those who want to keep up with the newest technologies and trending.\nSupported feed services:\nFeedly'
+  //}).show();
 }
