b093a50050bd9e61c135954755ecaa87c5beb8a6
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-04-17 22:09:40 -0700

    learn more wip

--- client/limily/app/controllers/learnMore.js
+++ client/limily/app/controllers/learnMore.js
@@ -6,6 +6,30 @@ function doClick(e) {
     backgroundColor: '#F7F7F7',
     top: Ti.Platform.displayCaps.platformHeight
   });
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'Welcome to Limily',
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'What is it?\nLimily is an inteligent news feed reader. It learns your preference and reading behavior over time. To save your time, Limily automagically brings the most relevant news at the top of the list. Limily is the best learning tool for those who want to keep up with the newest technologies and trending.\nSupported feed services:\nFeedly'
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'Limily Features',
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'Simple intuitive UI\nInteligent news selection from feeds',
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'Why do I need to login with Feedly?',
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'Because Limily needs a source of feeds and helps you with finding the most interesting news.',
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'More Information',
+  }));
+  ModalWindow.add(Ti.UI.createLabel({
+    text: 'Link to apple store.',
+  }));
   var slide = Titanium.UI.createAnimation();
   slide.top = 0;
   ModalWindow.open(slide);
@@ -15,10 +39,7 @@ function doClick(e) {
   close.addEventListener('click', function(e) {
     ModalWindow.close();
     ModalWindow = null;
+    $.destroy();
   });
   ModalWindow.add(close);
-  //Ti.UI.createAlertDialog({
-  //  title: 'Welcome to Limily',
-  //  message: 'What is it?\nLimily is an inteligent news feed reader. It learns your preference and reading behavior over time. To save your time, Limily automagically brings the most relevant news at the top of the list. Limily is the best learning tool for those who want to keep up with the newest technologies and trending.\nSupported feed services:\nFeedly'
-  //}).show();
 }
