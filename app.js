a3db476a428c1accdbcc5b4ccce1d7ae91826a4c
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-05-11 02:16:22 -0700

    graceful shutdown

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 
 var cluster = require('cluster');
+var log = require('util').log;
 if (cluster.isMaster) {
   var fs = require('fs');
   var stats = fs.statSync(__filename);
@@ -17,17 +18,21 @@ if (cluster.isMaster) {
       gid: stats.gid
     }).on('close', function(code) {
       if (code === 0) {
-        var newWorker = cluster.fork().on('listening', function() {
-          worker.kill();
-          worker = newWorker;
-        });
+        //var newWorker = cluster.fork().on('listening', function(w, address) {
+        //  log('A new server is listening to ' + address.address + ':' + address.port);
+        //  worker.kill();
+        //  worker = newWorker;
+        //});
+        worker.kill();
       }
     });
   },
   15 * 60 * 1000); // every 15min
   cluster.on('exit', function() {
     if (!Object.keys(cluster.workers).length) {
-      worker = cluster.fork();
+      worker = cluster.fork().on('listening', function(w, address) {
+        log('A new server is listening to ' + address.address + ':' + address.port);
+      });
     }
   });
 } else {
