e97df150604895258c84222fa7a50b63dd3bb88c
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-06-09 10:40:41 -0700

    term alert

--- client/limily/app/controllers/loginButton.js
+++ client/limily/app/controllers/loginButton.js
@@ -3,15 +3,37 @@ var url = args.resLocation || '/auth';
 var currentWindow = args.currentWindow;
 var getRecommends = require('getRecommends');
 
-function doClick(e) {
+var termAccepted = 'term20140609';
+
+var openLoginPage = function() {
   var webpage = Alloy.createController('webpage', url).getView();
-  //webpage.addEventListener('close', function(e) {
-  //  Ti.API.debug('closed');
-  //});
   webpage.addEventListener('authenticated', function(e) {
     getRecommends();
     currentWindow.fireEvent('openRows');
     Ti.API.debug('authenticated');
   });
   webpage.open();
+};
+
+function doClick(e) {
+  var termAccepted = Ti.App.Properties.getBool(termAccepted, false);
+  if (!termAccepted) {
+    var dialog = Ti.UI.createAlertDialog({
+      cancel: 0,
+      buttonNames: ['Cancel', 'Accept'],
+      message: 'aaa',
+      title: 'Privacy and Terms of Use'
+    });
+    dialog.addEventListener('click', function(e) {
+      if (e.index === e.source.cancel) {
+        Ti.API.info('The cancel button was clicked');
+        return;
+      }
+      Ti.App.Properties.setBool(termAccepted, true);
+      openLoginPage();
+    });
+    dialog.show();
+  } else {
+    openLoginPage();
+  }
 }
