a2bb9c357a60645656b95ba4ff1a8110c184db1c
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-06-20 00:36:23 -0700

    Revert "bring back localhost"
    This reverts commit 635fcf1293e15b13963920e65727e22c7c2755f9.

--- client/limily/app/controllers/webpage.js
+++ client/limily/app/controllers/webpage.js
@@ -110,11 +110,6 @@ if (html) {
   webview.setHtml(html);
 }
 webview.addEventListener('beforeload', function(e) {
-  // HACK for the sandbox enviroment
-  if (e.url && e.url.indexOf('http://localhost') === 0 && gBaseUrl.indexOf('localhost') === - 1) {
-    webview.setUrl(e.url.replace(/^http:\/\/localhost/, gBaseUrl));
-  }
-
   if (!webpage.noInd) {
     actInd.show();
   }
