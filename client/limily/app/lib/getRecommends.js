2821bea98d8ca9c5ee547ff0bf2f8861b66e508f
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-06-22 15:22:45 -0700

    consolidate getStars.js and getRecommends.js

--- client/limily/app/lib/getRecommends.js
+++ client/limily/app/lib/getRecommends.js
@@ -1,11 +1,12 @@
 var getReadIds = require('getReadIds');
-var getStars = require('getStars');
 var setImage = require('cacheImage').setImage;
-var url = gBaseUrl + '/recommends';
+var recommendUrl = gBaseUrl + '/recommends';
+var starUrl = gBaseUrl + '/stars';
 var recommends = Alloy.Collections.instance(DB);
 var TABLE = recommends.config.adapter.collection_name;
 var STATES = recommends.config.STATES;
 var unreadState = STATES.UNREAD;
+var starState = STATES.STAR;
 var client = Ti.Network.createHTTPClient({
   autoRedirect: false,
   timeout: 64 * 1000, // in milliseconds
@@ -18,14 +19,16 @@ var client = Ti.Network.createHTTPClient({
 function toThumb(blob) {
   return blob.imageAsThumbnail(90, 0, 0);
 }
-client.setOnload(function(e) { // on success
-  Ti.API.debug('sucess getReccomends');
+client.setOnload(function() { // on success
+  var url = this.location;
+  Ti.API.debug('sucess ' + url);
   if (this.status >= 400) { // workaround
     if (this.onerror) {
       this.onerror(e);
     }
     return;
   }
+  var defaultState = url === recommendUrl ? unreadState: starState;
   try {
     var items = JSON.parse(this.responseText).items;
     // UPSERT code http://stackoverflow.com/questions/418898/sqlite-upsert-not-insert-or-replace
@@ -35,7 +38,7 @@ client.setOnload(function(e) { // on success
     // by sorting by rowid, we can always get the newest sorted ranking
     var sql = ['INSERT OR REPLACE INTO', TABLE, '(id, state, data) VALUES '].join(' ');
     // http://stackoverflow.com/questions/1609637/is-it-possible-to-insert-multiple-rows-at-a-time-in-an-sqlite-database
-    var sqlVal = ['(?, COALESCE((SELECT state FROM ', TABLE, ' WHERE id = ?), ', unreadState , '), ?)'].join('');
+    var sqlVal = ['(?, COALESCE((SELECT state FROM ', TABLE, ' WHERE id = ?), ', defaultState, '), ?)'].join('');
     var sqls = [];
     var sqlArgs = [];
     for (var i = items.length; i--;) {
@@ -60,15 +63,15 @@ client.setOnload(function(e) { // on success
 });
 
 var getRecommends = function() {
-  //Ti.API.debug('getRecommends invoked!');
   // silently ignore this if there's no network connection
   if (!Ti.Network.online) {
     return;
   }
-  client.open('GET', url);
+  client.open('GET', recommendUrl);
   client.send();
   getReadIds();
-  getStars();
+  client.open('GET', starUrl);
+  client.send();
 };
 
 exports = getRecommends;
