4224b7af484782e12e0ee27fbf8606645bb3cb38
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-06-23 00:40:51 -0700

    Revert "consolidate getStars.js and getRecommends.js"
    This reverts commit 2821bea98d8ca9c5ee547ff0bf2f8861b66e508f.

--- client/limily/app/lib/getStars.js
+++ client/limily/app/lib/getStars.js
@@ -1,11 +1,9 @@
-var getReadIds = require('getReadIds');
-var getStars = require('getStars');
 var setImage = require('cacheImage').setImage;
-var url = gBaseUrl + '/recommends';
+var url = gBaseUrl + '/stars';
 var recommends = Alloy.Collections.instance(DB);
 var TABLE = recommends.config.adapter.collection_name;
 var STATES = recommends.config.STATES;
-var unreadState = STATES.UNREAD;
+var starState = STATES.STAR;
 var client = Ti.Network.createHTTPClient({
   autoRedirect: false,
   timeout: 64 * 1000, // in milliseconds
@@ -18,8 +16,8 @@ var client = Ti.Network.createHTTPClient({
 function toThumb(blob) {
   return blob.imageAsThumbnail(90, 0, 0);
 }
-client.setOnload(function(e) { // on success
-  Ti.API.debug('sucess getReccomends');
+client.setOnload(function() { // on success
+  Ti.API.debug('sucess getStars');
   if (this.status >= 400) { // workaround
     if (this.onerror) {
       this.onerror(e);
@@ -28,14 +26,9 @@ client.setOnload(function(e) { // on success
   }
   try {
     var items = JSON.parse(this.responseText).items;
-    // UPSERT code http://stackoverflow.com/questions/418898/sqlite-upsert-not-insert-or-replace
-    // the order of saving to sqlite is important
-    // the larger rowid, the newer (higher priority)
-    // experimentally confirmed that the save() monotonically increase its rowid even for existing row
-    // by sorting by rowid, we can always get the newest sorted ranking
+    // see lib/getRecommends.js
     var sql = ['INSERT OR REPLACE INTO', TABLE, '(id, state, data) VALUES '].join(' ');
-    // http://stackoverflow.com/questions/1609637/is-it-possible-to-insert-multiple-rows-at-a-time-in-an-sqlite-database
-    var sqlVal = ['(?, COALESCE((SELECT state FROM ', TABLE, ' WHERE id = ?), ', unreadState , '), ?)'].join('');
+    var sqlVal = ['(?, COALESCE((SELECT state FROM ', TABLE, ' WHERE id = ?), ', starState , '), ?)'].join('');
     var sqls = [];
     var sqlArgs = [];
     for (var i = items.length; i--;) {
@@ -59,16 +52,13 @@ client.setOnload(function(e) { // on success
   }
 });
 
-var getRecommends = function() {
-  //Ti.API.debug('getRecommends invoked!');
+var getStars = function() {
   // silently ignore this if there's no network connection
   if (!Ti.Network.online) {
     return;
   }
   client.open('GET', url);
   client.send();
-  getReadIds();
-  getStars();
 };
 
-exports = getRecommends;
+exports = getStars;
