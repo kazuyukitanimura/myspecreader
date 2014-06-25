aece87e4b85b208804e2404d6ec1eed768e45cdb
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-06-24 21:07:47 -0700

    touchstart

--- client/limily/app/controllers/menuIcon.js
+++ client/limily/app/controllers/menuIcon.js
@@ -2,7 +2,7 @@ var args = arguments[0] || {};
 var currentWindow = args.currentWindow;
 var menuIcon = $.menuIcon;
 
-menuIcon.addEventListener('click', function(e) {
+menuIcon.addEventListener('touchstart', function(e) {
   e.cancelBubble = true;
   Alloy.createController('menu', {
     parentWindow: currentWindow
