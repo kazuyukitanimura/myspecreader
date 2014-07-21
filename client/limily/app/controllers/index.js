2f7a1a20cb7751c6189ebe029fe1f06a40c80eea
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-07-21 08:49:44 -0700

    revert to 3.2.3GA since it does not have proper headers in http response, avoid unecessary http calls

--- client/limily/app/controllers/index.js
+++ client/limily/app/controllers/index.js
@@ -113,32 +113,32 @@ client.setOnload(function() { // on success
     index.needAuth = false;
     index.fireEvent('openRows');
     Ti.App.Properties.setBool(FIRST_TIME, false);
-  }
-  // setup background jobs
-  var bgOptions = {
-    url: 'getRecommends.js'
-  };
-  if (OS_ANDROID) {
-    // TODO debug this
-    var intent = Ti.Android.createServiceIntent(bgOptions);
-    intent.putExtra('interval', 10 * 60 * 1000); // every 10 min
-    var service = Ti.Android.createService(intent);
-    service.start();
-  } else {
-    if (OS_IOS) {
-      //Ti.App.iOS.registerBackgroundService(bgOptions);
-      // HACK
-      // fake the background job to run setInterval, once background service gets awake
-      // the setInterval of this (foreground) context also gets awake and executed
-      // so that require('getRecommends'); can access global Alloy
-      Ti.App.iOS.registerBackgroundService({
-        url: 'fake.js'
-      });
+    // setup background jobs
+    var bgOptions = {
+      url: 'getRecommends.js'
+    };
+    if (OS_ANDROID) {
+      // TODO debug this
+      var intent = Ti.Android.createServiceIntent(bgOptions);
+      intent.putExtra('interval', 10 * 60 * 1000); // every 10 min
+      var service = Ti.Android.createService(intent);
+      service.start();
+    } else {
+      if (OS_IOS) {
+        //Ti.App.iOS.registerBackgroundService(bgOptions);
+        // HACK
+        // fake the background job to run setInterval, once background service gets awake
+        // the setInterval of this (foreground) context also gets awake and executed
+        // so that require('getRecommends'); can access global Alloy
+        Ti.App.iOS.registerBackgroundService({
+          url: 'fake.js'
+        });
+      }
+      getRecommends();
+      clearInterval(intervalId);
+      intervalId = setInterval(getRecommends, 10 * 60 * 1000); // every 10 min
+      //intervalId = setInterval(getRecommends, 5 * 1000); // for test
     }
-    getRecommends();
-    clearInterval(intervalId);
-    intervalId = setInterval(getRecommends, 10 * 60 * 1000); // every 10 min
-    //intervalId = setInterval(getRecommends, 5 * 1000); // for test
   }
 });
 
@@ -168,7 +168,7 @@ index.addEventListener('openRows', _.debounce(function(e) {
   var i = 0;
   scrollView.stars = e.stars;
   var views = scrollView.views || [];
-  if (views.length && !views[0].sections[0].items.length) {
+  if (views.length && ! views[0].sections[0].items.length) {
     scrollView.shiftView();
     views = scrollView.views || [];
   }
@@ -205,7 +205,7 @@ index.addEventListener('openRows', _.debounce(function(e) {
           index.add(noMoreStars);
         }
       }
-      if (i < 2 && !e.stars) {
+      if (i < 2 && ! e.stars) {
         index.add(allRead);
         index.needAuth = true;
       }
@@ -218,7 +218,7 @@ index.addEventListener('openRows', _.debounce(function(e) {
   if (Ti.Network.online && index.needAuth) {
     client.ping(8 * 1000);
     return;
-  } else if (! Ti.Network.online && Ti.App.Properties.getBool(FIRST_TIME, true)) {
+  } else if (!Ti.Network.online && Ti.App.Properties.getBool(FIRST_TIME, true)) {
     Titanium.UI.createAlertDialog({
       title: 'Welcome, but Limily needs the network for the first time',
       message: 'Apologies. Limily failed getting online. Please check your Wifi / carrier signal. Also please make sure enabling your data network device settings.'
